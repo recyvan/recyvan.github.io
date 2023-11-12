3f6e380a5515885edb4e4d99cd9261bfc46ede8f
recyvan <e877586809@163.com>
2023-11-12 16:33:20 +0800

    Site updated: 2023-11-12 16:33:20

--- js/duration.js
+++ js/duration.js
@@ -5,7 +5,7 @@
     }
   
     // 计时起始时间，仿照格式自行修改
-    var start = Date.parse("2021-07-10T16:03:05");
+    var start = Date.parse("2022-12-20T15:03:05");
   
     function update() {
       var now = Date.now() + 250;
